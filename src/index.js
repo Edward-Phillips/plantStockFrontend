3007ff0a4d74de3c539081e32efd60a24e0f9c44
Edward Phillips <edd0ddi0@gmail.com>
2020-09-18 16:34:50 +0100

    add productForm to index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import ProductForm from './components/ProductForm';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ProductForm></ProductForm>
   </React.StrictMode>,
   document.getElementById('root')
 );
