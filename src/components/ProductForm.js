c1bed083679948a5e58174b95181917601b4c26e
Edward Phillips <edd0ddi0@gmail.com>
2020-09-21 19:38:55 +0100

    change from snake_case to CamelCase

--- src/components/ProductForm.js
+++ src/components/ProductForm.js
@@ -1,20 +1,20 @@
 import React, { useState } from 'react';
-import postData from '../api/apiCalls'
+import { postData } from '../api/apiCalls'
 
 const ProductForm  = () => {
-const [ product_name, setProduct_name ] = useState();
-  const [ cutting_type, setCutting_type ] = useState();
+const [ productName, setProductName ] = useState();
+  const [ cuttingType, setCuttingType ] = useState();
   
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const price = cutting_type == 'rooted' ? 3 : 1.5;
-    console.log(`product name: ${product_name}`);
-    console.log(`cutting_type: ${cutting_type}`);
+    const price = cuttingType == 'rooted' ? 3 : 1.5;
+    console.log(`product name: ${productName}`);
+    console.log(`cutting_type: ${cuttingType}`);
     console.log(`price: ${price}`);
 
     const data = { 
-      'product_name': product_name,
-      'cutting_type': cutting_type,
+      'product_name': productName,
+      'cutting_type': cuttingType,
       'price': price
     }
       const url = 'https://plantstock.herokuapp.com/v1/products'
@@ -29,13 +29,13 @@ const [ product_name, setProduct_name ] = useState();
   return (
     <form onSubmit={ handleSubmit }>
       <label for='product_name-input'>Product Name: </label>
-      <input type='text' className="product_name-input" placeholder='Enter plant name here' value={product_name} onChange={(e) => setProduct_name(e.target.value)}></input>
+      <input type='text' className="product_name-input" placeholder='Enter plant name here' value={productName} onChange={(e) => setProductName(e.target.value)}></input>
       <br></br>
       <label for='rooted'>cutting type: rooted</label>
-      <input type='radio' id='rooted' className='cutting_type-input' name='cutting_type' value='rooted' checked={cutting_type === 'rooted'} onChange={(e) => setCutting_type(e.target.value)}></input>
+      <input type='radio' id='rooted' className='cutting_type-input' name='cuttingType' value='rooted' checked={cuttingType === 'rooted'} onChange={(e) => setCuttingType(e.target.value)}></input>
       <br></br>
       <label for='unrooted'>cutting type: unrooted</label>
-      <input type='radio' id='unrooted' className='cutting_type-input' name='cutting_type' value='unrooted' checked={cutting_type === 'unrooted'} onChange={(e) => setCutting_type(e.target.value)}></input>
+      <input type='radio' id='unrooted' className='cutting_type-input' name='cuttingType' value='unrooted' checked={cuttingType === 'unrooted'} onChange={(e) => setCuttingType(e.target.value)}></input>
       <input type='submit'></input>
     </form>
   )
