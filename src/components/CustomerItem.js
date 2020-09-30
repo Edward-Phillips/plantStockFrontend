779f6e7686152ebc21cc3aab2303245cf448e789
Edward Phillips <edd0ddi0@gmail.com>
2020-09-30 14:50:58 +0100

    add component for each customer in table

---
+++ src/components/CustomerItem.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const CustomerItem = ({customer}) => {
+
+  return (
+    <tr className='row' key={customer.id}>
+          <td className='nameCell' key={customer.id}>{ customer.name }</td>
+          <td className='addressCell' key={customer.id}>{ customer.address }</td>
+          </tr>
+  )
+}
+
+export default CustomerItem
