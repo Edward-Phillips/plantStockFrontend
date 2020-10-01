bba8137ab5be6d6b62f2c5e876af91b14dc369f7
Edward Phillips <edd0ddi0@gmail.com>
2020-10-01 20:36:23 +0100

    Refactor Navbar so that page refresh does not return 404

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,6 +1,4 @@
-import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
-import { Switch, Route, Link } from 'react-router-dom';
+import React, { useState } from 'react';
 import ProductForm from '../ProductForm';
 import ProductPage from '../ProductPage';
 import StockPage from '../StockPage';
@@ -11,49 +9,46 @@ import Home from '../Home';
 import './NavBar.css';
 
 const NavBar = () => {
+  const [currentPage, setCurrentPage] = useState('Home');
+
+  const switchComponent = (componentName) => {
+    switch (componentName) {
+      case 'Home':
+        return <Home/>
+      case 'ProductForm':
+        return <ProductForm/>
+      case 'ProductPage':
+        return <ProductPage/>
+      case 'StockForm':
+        return <StockForm/>
+      case 'StockPage':
+        return <StockPage/>
+      case 'CustomerForm':
+        return <CustomerForm/>
+      case 'CustomerPage':
+        return <CustomerPage/>
+      default:
+        return <Home/>
+        break;
+    }
+  }
+
   return (
     <div>
-    <div id="bar">
-      <Navbar bg="light" variant="light">
-        <Navbar.Brand as={Link} to="/">
-          PlantStock
-        </Navbar.Brand>
-        <Nav className="ml-auto">
-          <Nav.Link className='navItem' as={Link} to="/product-entry">
-            Enter new Product
-          </Nav.Link>
-          <Nav.Link className='navItem' as={Link} to="/view-products">
-            View Products
-          </Nav.Link>
-          <Nav.Link className='navItem' as={Link} to="/stock-entry">
-            Enter new Stock
-          </Nav.Link>
-          <Nav.Link className='navItem' as={Link} to="/view-stock">
-            View Stock
-          </Nav.Link>
-          <Nav.Link className='navItem' as={Link} to="/customer-entry">
-            Add new Customer
-          </Nav.Link>
-          <Nav.Link className='navItem' as={Link} to="/view-customers">
-            View Customers
-          </Nav.Link>
-        </Nav>
-      </Navbar>
+      <div id='navbar' className='navigation'>
+        <button value='Home' onClick={(e) => {setCurrentPage(e.target.value)}}>PlantStock</button>
+        <button value='ProductForm' onClick={(e) => {setCurrentPage(e.target.value)}}>Add new Product</button>
+        <button value='ProductPage' onClick={(e) => {setCurrentPage(e.target.value)}}>View Products</button>
+        <button value='StockForm' onClick={(e) => {setCurrentPage(e.target.value)}}>Add new Stock</button>
+        <button value='StockPage' onClick={(e) => {setCurrentPage(e.target.value)}}>View Stock</button>
+        <button value='CustomerForm' onClick={(e) => {setCurrentPage(e.target.value)}}>Add new Customer</button>
+        <button value='CustomerPage' onClick={(e) => {setCurrentPage(e.target.value)}}>View Customers</button>
+      </div>
+      <div className="switch">
+        {switchComponent(currentPage)}
+      </div>
     </div>
-    <div>
-      <Switch className='switch'>
-        <Route exact path="/" component={Home} />
-        <Route path="/product-entry" component={ProductForm} />
-        <Route path="/view-products" component={ProductPage} />
-        <Route path="/view-stock" component={StockPage} />
-        <Route path="/stock-entry" component={StockForm} />
-        <Route path="/customer-entry" component={CustomerForm} />
-        <Route path="/view-customers" component={CustomerPage} />
-        <Route path="*" component={Home} />
-      </Switch>
-    </div>
-  </div>
   )
-};
+}
 
 export default NavBar
