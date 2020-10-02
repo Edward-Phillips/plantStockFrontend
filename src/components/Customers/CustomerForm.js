c8bf2dcdaba6d8b177aff3ae6c655cc6f78e224f
Edward Phillips <edd0ddi0@gmail.com>
2020-10-02 17:26:38 +0100

    add Styling for forms

--- src/components/Customers/CustomerForm.js
+++ src/components/Customers/CustomerForm.js
@@ -21,10 +21,8 @@ const CustomerForm  = () => {
     <form onSubmit={ handleSubmit }>
       <label htmlFor='name-input'>Customer Name: </label>
       <input type='text' className="name-input" placeholder='Enter Customer name here' value={name} onChange={(e) => setName(e.target.value)}></input>
-      <br></br>
       <label htmlFor='address-input'>Customer Address:  </label>
       <input type='text' className='address-input' placeholder='WHERE DO THEY LIVE' value={address} onChange={(e) => setAddress(e.target.value)}></input>
-      <br></br>
       <input type='submit'></input>
     </form>
   )
---
+++ src/components/Orders/OrderForm.js
@@ -0,0 +1,8 @@
+import React, { useState, useEffect } from 'react';
+import { getData, postData } from '../../api/apiCalls';
+
+const OrderForm = (customer) => {
+
+  const [availableStock, setAvailableStock] = useState([]);
+  
+}
