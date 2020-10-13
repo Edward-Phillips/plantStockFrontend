9f6a3a960f79b53cd42580d6a75b2d4e13f97f63
Edward Phillips <edd0ddi0@gmail.com>
2020-10-13 16:22:23 +0100

    Change CustomerPage to CustomerTable

---
+++ src/components/Customers/CustomerTable.js
@@ -0,0 +1,51 @@
+import React, { useState, useEffect } from 'react';
+import { getData } from '../../api/apiCalls';
+import CustomerItem from './CustomerItem';
+import Spinner from 'react-bootstrap/Spinner';
+import Table from 'react-bootstrap/Table';
+import './CustomerTable.css';
+
+const CustomerTable = (props) => {
+  const [customersArray, setCustomersArray] = useState([]);
+  const [fetchStatus, setFetchStatus] = useState(true);
+
+  useEffect( () => {
+    const retrieveResults = async () => {
+      const url = 'https://plantstock.herokuapp.com/v1/customers';
+      const result = await (await getData(url));
+      setCustomersArray(result.customers);
+      setFetchStatus(false);
+    }
+    retrieveResults()
+  },[]);
+
+  return (
+    <>
+      <h1>Customer Table</h1>
+      {
+        fetchStatus ?
+          <Spinner animation="border" role="status" className='spinner'><span className="sr-only spinner">Loading...</span></Spinner>
+        :
+          <Table className='customer' striped bordered size="md">
+            <thead>
+              <tr className="customer">
+                <th>Customer Name</th>
+                <th>Customer Address</th>
+              </tr>
+            </thead>
+            <tbody>
+                {
+                  customersArray.map((customer, index) => {
+                      return (
+                        <CustomerItem customer={customer} personalCustomerHandler={props.personalCustomerHandler} key={index}/>
+                      )
+                    })
+                }
+            </tbody>
+          </Table>
+      }
+    </>
+  )
+}
+
+export default CustomerTable
