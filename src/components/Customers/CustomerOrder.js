02c1ca0628617309e2ac5d1216c6d3bc943c1984
Edward Phillips <edd0ddi0@gmail.com>
2020-10-14 12:18:34 +0100

    remove console log

--- src/components/Customers/CustomerOrder.js
+++ src/components/Customers/CustomerOrder.js
@@ -1,9 +1,6 @@
 import React, { useEffect } from 'react';
 
 const CustomerOrder = (props) => {
-  useEffect(() => {
-    console.log(props.order);
-  })
   return (
     <tr key={props.order.id}>
       <td className='dateCell'>{props.order.order_date}</td>
