a07a9825baaad47f728d9c7f8007619b9d81d1a3
Edward-Phillips <edd0ddi0@gmail.com>
2021-05-08 13:37:41 +0100

    use SpinnerTable

--- src/components/Stock/StockPage.js
+++ src/components/Stock/StockPage.js
@@ -1,8 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { getData } from '../../api/apiCalls';
 import StockItem from './StockItem';
-import Spinner from 'react-bootstrap/Spinner';
-import Table from 'react-bootstrap/Table';
+import SpinnerTable from '../Miscellaneous/SpinnerTable'
 import './StockPage.css';
 
 const StockPage = () => {
@@ -22,30 +21,7 @@ const StockPage = () => {
   return (
     <>
     <h1>Current Stock</h1>
-    {
-      fetchStatus ?
-        <Spinner animation="border" role="status" className='spinner'><span className="sr-only spinner">Loading...</span></Spinner>
-      :
-        <Table striped bordered size="sm">
-        <thead>
-          <tr className="stock">
-            <th>Product Name</th>
-            <th>Cutting Type</th>
-            <th>Cost Per Cutting</th>
-            <th>Current Count</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            stockArray.map((stock, index) => {
-              return (
-                <StockItem stock={stock} key={index}/>
-              )
-            })
-          }
-        </tbody>
-        </Table>
-    }
+    <SpinnerTable headers={['Product Name', 'Cutting Type', 'Cost Per Cutting','Current Count']} fetchStatus={ fetchStatus } contents={ stockArray } wrapper={StockItem} />
     </>
   )
 }
